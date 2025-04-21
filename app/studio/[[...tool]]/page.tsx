7be042b8dba4944ab018a7c764a980c515eb106d
William Lowrimore <wlowrimore@gmail.com>
2025-04-20 19:10:24 -0500

    updating studio/[[...tool]]/page.tsx

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -7,13 +7,30 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+// import { NextStudio } from 'next-sanity/studio'
+// import config from '../../../sanity.config'
 
-export const dynamic = 'force-static'
+// export const dynamic = 'force-static'
 
-export { metadata, viewport } from 'next-sanity/studio'
+// export { metadata, viewport } from 'next-sanity/studio'
 
-export default function StudioPage() {
-  return <NextStudio config={config} />
+// export default function StudioPage() {
+//   return <NextStudio config={config} />
+// }
+
+import { auth } from "@/auth";
+import { NextStudio } from "next-sanity/studio";
+import { redirect } from "next/navigation";
+import config from "../../../sanity.config";
+
+export const dynamic = "force-dynamic";
+
+export default async function StudioPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
+  return <NextStudio config={config} />;
 }
