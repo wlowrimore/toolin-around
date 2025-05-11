92c81d7b8088219539e41154ffe9ed569b763daf
William Lowrimore <wlowrimore@gmail.com>
2025-05-11 10:21:42 -0500

    changes the import of env variables

--- sanity.config.ts
+++ sanity.config.ts
@@ -19,8 +19,8 @@ const apiVersion = process.env.NEXT_PUBLIC_API_VERSION || "2024-01-01";
 
 export default defineConfig({
   basePath: "/studio",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+  dataset: process.env.SANITY_STUDIO_DATASET || "production",
   apiVersion,
   schema,
   plugins: [
